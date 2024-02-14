9ab5d2918d7555b7fda9689edc1752d23659fac7
gwenLemur <gwendolynjo@gmail.com>
2024-02-14 10:55:22 -0800

    day1

--- app/src/main/java/com/example/catapi/CatListActivity.kt
+++ app/src/main/java/com/example/catapi/CatListActivity.kt
@@ -2,8 +2,11 @@ package com.example.catapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.catapi.databinding.ActivityCatlistBinding
 
 class CatListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCatlistBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catlist)
