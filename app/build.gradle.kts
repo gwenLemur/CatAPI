1deba20e12b09b12879e212068173af08a9e58b1
gwenLemur <gwendolynjo@gmail.com>
2024-02-14 10:51:53 -0800

    day1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,20 @@ plugins {
 
 android {
     namespace = "com.example.catapi"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.catapi"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 
     buildTypes {
@@ -39,8 +43,9 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/catapi/CatAdapterActivity.kt
@@ -0,0 +1,4 @@
+package com.example.catapi
+
+class CatAdapterActivity {
+}
