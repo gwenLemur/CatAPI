398ecb6ddb13c178d38fc8e23c919fa1b14d230b
saltine1 <sophialiu.mail@gmail.com>
2024-02-20 09:54:51 -0800

    day 2: get api call and make adapter (unfinished)

--- app/src/main/java/com/example/catapi/CatAdapterActivity.kt
+++ app/src/main/java/com/example/catapi/CatAdapterActivity.kt
@@ -1,5 +1,41 @@
 package com.example.catapi
 
-class CatAdapterActivity {
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.catapi.models.CatObj
 
+class CatAdapterActivity(var catList: MutableList<CatObj>) :
+    RecyclerView.Adapter<CatAdapterActivity.ViewHolder>(){
+        class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+            val textViewCodes: TextView
+            val textViewTitle: TextView
+            val textViewUrl: TextView
+            val layout: ConstraintLayout
+
+            init{
+                textViewCodes = view.findViewById(R.id.code)
+                textViewTitle = view.findViewById(R.id.title)
+                textViewUrl = view.findViewById(R.id.url)
+                layout = view.findViewById(R.id.layout)
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+            return ViewHolder(view)
+        }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val cats = catList[position]
+        val context = holder.layout.context
+        holder.textViewCodes.text = cats.status_code.toString()
+    }
+
+    override fun getItemCount() = catList.size
 }
+
--- app/src/main/java/com/example/catapi/CatListActivity.kt
+++ app/src/main/java/com/example/catapi/CatListActivity.kt
@@ -2,15 +2,39 @@ package com.example.catapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.catapi.api.CatService
+import com.example.catapi.api.RetrofitHelper
 import com.example.catapi.databinding.ActivityCatlistBinding
+import com.example.catapi.models.CatObj
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CatListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCatlistBinding
+    private lateinit var adapter: CatAdapterActivity
+    private lateinit var cats: CatObj
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //#TODO: Set up class architecture, get API call, set up adapter, view actvity
         //i did architecture but like ??
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_catlist)
+        binding = ActivityCatlistBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val catService = RetrofitHelper.getInstance().create(CatService::class.java)
+        val catCall = catService.getCatData()
+
+        catCall.enqueue(object: Callback<CatObj>{
+            override fun onResponse(
+                call: Call<CatObj>,
+                response: Response<CatObj>
+            ){
+                cats = response.body()!!
+
+
+            }
+        }
     }
 }
