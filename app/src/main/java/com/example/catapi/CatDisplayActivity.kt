09bc18663c34f4bb0532ccfdcf79bbbbbcb92390
saltine1 <sophialiu.mail@gmail.com>
2024-02-26 09:20:55 -0800

    day 4: working catdisplayactivity except for the image

--- app/src/main/java/com/example/catapi/CatDisplayActivity.kt
+++ app/src/main/java/com/example/catapi/CatDisplayActivity.kt
@@ -4,22 +4,33 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.catapi.databinding.ActivityDisplayBinding
 import com.example.catapi.models.CatObj
-//
-//class CatDisplayActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityDisplayBinding
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityDisplayBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        val context = binding.layout.context
-//        val currCat = intent.getParcelableExtra<CatObj>("currCat" )
-//
-//
-//
-//    }
-//
-//
-//
-//}
+import com.squareup.picasso.Picasso
+
+class CatDisplayActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDisplayBinding
+
+    companion object {
+        val EXTRA_CAT = "currCat"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDisplayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var parcel = intent.getParcelableExtra<CatObj>(EXTRA_CAT)
+        var image = parcel?.url
+        var title = parcel?.title
+        var code = parcel?.status_code
+
+        val context = binding.layout.context
+        val currCat = intent.getParcelableExtra<CatObj>("currCat" )
+        binding.title.text = title
+        binding.code.text = code.toString()
+        Picasso.get().load(image).into(binding.image)
+
+    }
+
+
+
+}
